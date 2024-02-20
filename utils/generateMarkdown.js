042cd67dcb64ec4c1b9d5b9ebac5bb84d19825b7
unknown <kaleimbusiness@gmail.com>
2024-02-20 16:19:38 +0000

    Added Javascript to GenerateMarkdown file to create a profressional layout and meeting acceptance criteria

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,28 @@
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `
+  # ${data.title}
+  ${data.Description}
+  ## Table of Contents
+  * [Usage](#usage)
+  * [License](#license)
+  * [Credits](#credits)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  ${data.install}
+  ## Usage 
+  To use this app you will have to, ${data.usage}
+  ## Credits
+  ${data.credits}
+  ## License
+  This Project is protected under this project - ${data.license}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  * Email - ${data.email}
+  * Github Profile - [${data.github}](https://github.com/${data.github})
+  `;
 }
 
-module.exports = generateMarkdown;
+export default generateMarkdown;
